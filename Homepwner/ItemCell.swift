f0bd7e3a3c575dd9d17fa8497192dacdad005351
Edouard Barbier <edouardbarbier0@gmail.com>
2017-04-20 22:19:47 +0100

    Added Dynamic Type support

--- Homepwner/ItemCell.swift
+++ Homepwner/ItemCell.swift
@@ -13,5 +13,14 @@ class ItemCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var serialNumberLabel: UILabel!
     @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+        
+    }
         
 }
