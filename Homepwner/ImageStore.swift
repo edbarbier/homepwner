ffc3d524cbf25998b2dea782ebc83273aa2dc16b
Edouard Barbier <edouardbarbier0@gmail.com>
2017-04-25 23:53:20 +0100

    Application saves Images for item in FileManager

--- Homepwner/ImageStore.swift
+++ Homepwner/ImageStore.swift
@@ -19,17 +19,47 @@ class ImageStore {
     
         cache.setObject(image, forKey: key as NSString)
         
+        let url = imageURL(forKey: key)
+        
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            let _ = try? data.write(to: url, options: [.atomic])
+        }
+        
     }
     
     func image(forKey key: String) -> UIImage? {
         
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage
+        }
         
-        return cache.object(forKey: key as NSString)
+        let url = imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path)
+            else {
+                return nil
+        }
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
+        
     }
     
     func deleteImage(forKey key: String) {
         
         cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        do {
+            try FileManager.default.removeItem(at: url)
+        } catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent(key)
     }
     
     
